c29186689c71d46b2b695770531d713f0840bb43
Dominic Rooney <orangutanrider@gmail.com>
2023-06-30 20:41:37 +0100

    Player script updates

--- ShmupMusicisian-UnityProj/Assets/Game Files/Scripts/Player/BasicPlayerBulletScript.cs
+++ ShmupMusicisian-UnityProj/Assets/Game Files/Scripts/Player/BasicPlayerBulletScript.cs
@@ -6,11 +6,7 @@ public class BasicPlayerBulletScript : MonoBehaviour
 {
     [Header("Required References")]
     public Rigidbody2D rb2D;
-    public BasicPlayerBulletParams bulletParams;
-
-    static Transform firingPointA = null;
-    static Transform firingPointB = null;
-    static bool firingPointABFlipFlop = false; // false is A, true is B
+    [SerializeField] BasicPlayerBulletParams bulletParams;
 
     public bool Active
     {
@@ -21,6 +17,14 @@ public class BasicPlayerBulletScript : MonoBehaviour
     }
     bool active = true;
 
+    // initialization data
+    static bool initialized = false;
+    static bool firingPointABFlipFlop = false; // false is A, true is B
+    static Transform firingPointA = null;
+    static Transform firingPointB = null;
+    static GameObject playerObject;
+    static EnemyHitManager hitManager;
+
     void Start()
     {
         DeActivateBullet();
@@ -28,35 +32,46 @@ public class BasicPlayerBulletScript : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        // get params
-        LayerMask enemyMask = bulletParams.enemyMask;
-        LayerMask bulletBoundaryMask = bulletParams.bulletBoundaryMask;
-
-        // check if collision is in mask
-        int layer = collision.gameObject.layer;
-        if ((enemyMask & (1 << layer)) != 0)
-        {
-            return;
-        }
-        if ((bulletBoundaryMask & (1 << layer)) != 0)
-        {
-            DeActivateBullet();
-            return;
-        }
+        HandleBulletCollision(collision.collider);
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        HandleBulletCollision(collision);
+    }
+
+    public void ShootBullet()
+    {
+        if (initialized == false)
+        {
+            Debug.LogWarning("BasicBullets haven't been initialized, a BasicBulletManager is usually what does this.");
+            DeActivateBullet();
+            return;
+        }
+
+        if (active == true)
+        {
+            Debug.LogWarning("BasicBullet is currently active and fired");
+            return;
+        }
+        ActivateBullet();
+    }
+
+    void HandleBulletCollision(Collider2D collision)
+    {
         // get params
         LayerMask enemyMask = bulletParams.enemyMask;
         LayerMask bulletBoundaryMask = bulletParams.bulletBoundaryMask;
 
-        // check if collision is in mask
         int layer = collision.gameObject.layer;
+
+        // if enemy hit
         if ((enemyMask & (1 << layer)) != 0)
         {
+            BullletEnemyHit(collision);
             return;
         }
+        // if boundary hit
         if ((bulletBoundaryMask & (1 << layer)) != 0)
         {
             DeActivateBullet();
@@ -64,25 +79,38 @@ public class BasicPlayerBulletScript : MonoBehaviour
         }
     }
 
-    public static void InitializeBullets(Transform _firingPointA, Transform _firingPointB)
+    void BullletEnemyHit(Collider2D collision)
     {
-        firingPointA = _firingPointA;
-        firingPointA = _firingPointB;
-    }
-
-    public void ShootBullet()
-    {
-        if(active == true) 
+        if(initialized == false)
         {
-            Debug.LogWarning("Bullet is currently active and fired");
-            return; 
+            Debug.LogWarning("BasicBullets haven't been initialized, a BasicBulletManager is usually what does this.");
+            DeActivateBullet();
+            return;
         }
-        ActivateBullet();
+
+        // get enemy status
+        EnemyStatusChanger enemyStatus = hitManager.GetEnemyStatusViaFormattedName(collision.gameObject.name);
+        if(enemyStatus == null) { return; }
+
+        // get params
+        float damage = bulletParams.damage;
+
+        // construct damage data
+        AtEnemyDamageData damageData = new AtEnemyDamageData(damage, Vector2.up, transform.position, playerObject);
+
+        // apply damage
+        enemyStatus.ApplyDamage(damageData);
     }
 
-    void BullletEnemyHit()
+    #region Object Pooling Stuff
+    public static void InitializeBullets(Transform _firingPointA, Transform _firingPointB, EnemyHitManager _hitManager, GameObject _playerObject)
     {
+        initialized = true;
 
+        firingPointA = _firingPointA;
+        firingPointB = _firingPointB;
+        hitManager = _hitManager;
+        playerObject = _playerObject;
     }
 
     void ActivateBullet()
@@ -98,22 +126,23 @@ public class BasicPlayerBulletScript : MonoBehaviour
         firingPointABFlipFlop = !firingPointABFlipFlop;
         if (firingPointABFlipFlop == false)
         {
-            rb2D.MovePosition(firingPointA.position);
-            //transform.position = firingPointA.position;
+            //rb2D.MovePosition(firingPointA.position);
+            transform.position = firingPointA.position;
         }
         else
         {
-            rb2D.MovePosition(firingPointB.position);
-            //transform.position = firingPointB.position;
+            //rb2D.MovePosition(firingPointB.position);
+            transform.position = firingPointB.position;
         }
 
         // give it velocity
         rb2D.velocity = Vector2.up * speed;
     }
 
-    void DeActivateBullet()
+    public void DeActivateBullet()
     {
         active = false;
         gameObject.SetActive(false);
     }
+    #endregion
 }
--- ShmupMusicisian-UnityProj/Assets/Game Files/Scripts/Player/PlayerBasicWeapons.cs
+++ ShmupMusicisian-UnityProj/Assets/Game Files/Scripts/Player/PlayerBasicWeapons.cs
@@ -1,18 +1,68 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerBasicWeapons : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("Required References")]
+    [SerializeField] BasicPlayerBulletParams weaponParams;
+    public PlayerBasicBulletManager bulletManager;
+
+    bool firing = false;
+    float shotTimer = 0f;
+
+    private void Start()
     {
-        
+        shotTimer = weaponParams.shotInterval;
     }
 
-    // Update is called once per frame
-    void Update()
+    // Execution
+    private void FixedUpdate()
     {
-        
+        FiringUpdate();
+    }
+
+    private void OnDisable()
+    {
+        ResetFlags();
+    }
+
+    // Input event
+    public void FireBasicWeaponsInput(InputAction.CallbackContext context)
+    {
+        switch (context.phase)
+        {
+            case InputActionPhase.Performed:
+                firing = true;
+                break;
+            case InputActionPhase.Canceled:
+                firing = false;
+                break;
+        }
+    }
+
+    // Fixed Update function
+    void FiringUpdate()
+    {
+        if (firing == false)
+        {
+            shotTimer = weaponParams.shotInterval;
+            return;
+        }
+
+        shotTimer = shotTimer + Time.fixedDeltaTime;
+
+        if (shotTimer < weaponParams.shotInterval) { return; }
+
+        // Shoot Bullet
+        bulletManager.ShootBullet();
+        shotTimer = 0f;
+    }
+
+    public void ResetFlags()
+    {
+        firing = false;
+        shotTimer = weaponParams.shotInterval;
     }
 }
