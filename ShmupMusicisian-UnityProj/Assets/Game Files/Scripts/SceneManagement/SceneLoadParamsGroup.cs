eb3c3b4b481473d65036534294982dbd73d09772
Dominic Rooney <orangutanrider@gmail.com>
2023-07-06 17:38:52 +0100

    Old system deleted
    This wasn't done to replace the old system; The new system is basically the same. I just deleted the old scripts because I wanted to change the naming convention.

--- ShmupMusicisian-UnityProj/Assets/Game Files/Scripts/SceneManagement/SceneLoadParamsGroup.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "SceneLoadParamsGroup", menuName = "SceneManagement/ParamsGroup")]
-public class SceneLoadParamsGroup : ScriptableObject
-{
-    public List<SceneLoadParams> paramsGroup = new List<SceneLoadParams>();
-}
