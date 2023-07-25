bad179bd243ddd55d74bdd9a0a73dd1e53816f39
Dominic Rooney <orangutanrider@gmail.com>
2023-07-25 23:31:07 +0100

    Emitter Groups
    I made these to keep things tidy in the animation window (which is how I'm creating the attack patterns).
    Basically, it just groups a bunch of emitters under one monobehaviour, and then you can call emit on that group to emit all groups at once.
    Then there are these bonus scripts, the AnimEventLink ones. They just exist because animation events can't call on scripts on child gameobjects, and I want the groups to be on child gameobjects for organisation reasons. So, the link things just store a reference to a group and can then be called by the animation event; A middle man.

---
+++ ShmupMusicisian-UnityProj/Assets/Game Files/Scripts/Enemy/Emitter/EEmitterGroup.cs
@@ -0,0 +1,103 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EEmitterGroup : MonoBehaviour
+{
+    [System.Serializable]
+    private class GroupedEmitter
+    {
+        public GroupedEmitter(EEmitter _emitter, EEmitterGroup _parent, float _zRotationOnStart, float _zRotationRandomOffset = 0, float _randomDelay = 0)
+        {
+            emitter = _emitter;
+            Parent = _parent;
+            ZRotationOnStart = _zRotationOnStart;
+
+            randomDelay = _randomDelay;
+            zRotationRandomOffset = _zRotationRandomOffset;
+        }
+
+        public EEmitter emitter;
+        [Space]
+        public float zRotationRandomOffset = 0;
+        public float randomDelay = 0;
+
+        public float ZRotationOnStart { get; set; }
+        public EEmitterGroup Parent { get; set; }
+
+        public void Emit()
+        {
+            float delay = Random.Range(0, randomDelay);
+
+            if (delay <= 0)
+            {
+                float randomOffset = Random.Range(-zRotationRandomOffset, zRotationRandomOffset);
+                Vector3 newEulerRotation = new Vector3(0, 0, ZRotationOnStart + randomOffset);
+                emitter.transform.eulerAngles = newEulerRotation;
+                emitter.Emit();
+            }
+            else
+            {
+                Parent.StartCoroutine(EmitDelayed(delay));
+            }
+        }
+
+        public IEnumerator EmitDelayed(float delay)
+        {
+            yield return new WaitForSeconds(delay);
+
+            float randomOffset = Random.Range(-zRotationRandomOffset, zRotationRandomOffset);
+            Vector3 newEulerRotation = new Vector3(0, 0, ZRotationOnStart + randomOffset);
+            emitter.transform.eulerAngles = newEulerRotation;
+            emitter.Emit();
+            yield break;
+        }
+    }
+
+    [SerializeField] List<GroupedEmitter> emitters = new List<GroupedEmitter>();
+
+    private void Start()
+    {
+        foreach(GroupedEmitter emitter in emitters)
+        {
+            emitter.ZRotationOnStart = emitter.emitter.transform.eulerAngles.z;
+            emitter.Parent = this;
+        }
+    }
+
+    public void EmitAll()
+    {
+        foreach (GroupedEmitter emitter in emitters)
+        {
+            emitter.Emit();
+        }
+    }
+
+    public void MultiEditRandomRotation(float value)
+    {
+        foreach (GroupedEmitter emitter in emitters)
+        {
+            emitter.zRotationRandomOffset = value;
+        }
+    }
+
+    public void MultiEditRandomDelay(float value)
+    {
+        foreach (GroupedEmitter emitter in emitters)
+        {
+            emitter.randomDelay = value;
+        }
+    }
+
+    public void GetEEmittersFromChildren()
+    {
+        EEmitter[] newEmitters = gameObject.GetComponentsInChildren<EEmitter>();
+
+        emitters = new List<GroupedEmitter>();
+        foreach (EEmitter emitter in newEmitters)
+        {
+            GroupedEmitter groupedEmitter = new GroupedEmitter(emitter, this, emitter.transform.eulerAngles.z);
+            emitters.Add(groupedEmitter);
+        }
+    }
+}
